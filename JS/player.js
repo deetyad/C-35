caa1c83c62d525edc3df170136b5bcc32c4d7393
deetyad <68077521+deetyad@users.noreply.github.com>
2020-10-11 13:28:30 -0500

    Add files via upload

---
+++ JS/player.js
@@ -0,0 +1,24 @@
+class Player{
+    constructor(){
+
+    }
+
+    getCount(){
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value", function(data){
+            playerCount = data.val();
+        });
+    }
+
+    updateCount(count){
+        database.ref('/').update({
+            playerCount: count
+        });
+    }
+
+    update(name){
+        var playerIndex = database.ref("player" + playerCount).set({
+            name: name
+        });
+    }
+}
